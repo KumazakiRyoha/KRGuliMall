3b3ce455d285db9ce4c12e3b060f0e5c12da87c1
rutaceae <1593713783@qq.com>
2022-09-02 16:03:28 +0800

    项目初始结构创建

---
+++ guliMall-wave/src/main/java/com/atguigu/gulimall/wave/GuliMallWaveApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.wave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliMallWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliMallWaveApplication.class, args);
+    }
+
+}
